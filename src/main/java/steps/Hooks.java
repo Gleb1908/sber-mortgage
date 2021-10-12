df6f6c8f3f28a41a38597dcae8eb44497c4189e5
Gleb1908 <gleb1908@list.ru>
2021-10-12 12:19:33 +0300

    added Hooks.feature

---
+++ src/main/java/steps/Hooks.java
@@ -0,0 +1,20 @@
+package steps;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import managers.InitManager;
+
+public class Hooks {
+
+    @Before
+    public void before() {
+        InitManager.initFramework();
+    }
+
+
+    @After
+    public void after() {
+        InitManager.quitFramework();
+    }
+
+}
