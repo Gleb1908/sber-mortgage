a3a00c66855ff8e32b4b6df053a8c69840d210f0
Gleb1908 <gleb1908@list.ru>
2021-10-12 12:21:33 +0300

    added PageManager.feature

---
+++ src/main/java/managers/PageManager.java
@@ -0,0 +1,68 @@
+package managers;
+
+import Pages.HomePage;
+import Pages.MortgagePage;
+
+public class PageManager {
+
+    /**
+     * Менеджер страничек
+     */
+    private static PageManager pageManager;
+
+    /**
+     * Стартовая страничка
+     */
+    private HomePage homePage;
+
+    /**
+     * Страничка с результатами поиска
+     */
+    private MortgagePage mortgagePage;
+
+    /**
+     * Конструктор специально был объявлен как private (singleton паттерн)
+     *
+     * @see PageManager#getPageManager()
+     */
+    private PageManager() {
+    }
+
+    /**
+     * Ленивая инициализация PageManager
+     *
+     * @return PageManager
+     */
+    public static PageManager getPageManager() {
+        if (pageManager == null) {
+            pageManager = new PageManager();
+        }
+        return pageManager;
+    }
+
+    /**
+     * Ленивая инициализация {@link HomePage}
+     *
+     * @return HomePage
+     */
+    public HomePage getHomePage() {
+        if (homePage == null) {
+            homePage = new HomePage();
+        }
+        return homePage;
+    }
+
+    /**
+     * Ленивая инициализация {@link MortgagePage}
+     *
+     * @return ResultsPage
+     */
+    public MortgagePage getMortgagePage() {
+        if (mortgagePage == null) {
+            mortgagePage = new MortgagePage();
+        }
+        return mortgagePage;
+    }
+
+
+}
