7bd078e145189d8bc54b7cd7bda1d3f4111b0bb8
Gleb1908 <gleb1908@list.ru>
2021-10-12 12:20:56 +0300

    added MortgageTest.feature

---
+++ src/test/java/tests/MortgageTest.java
@@ -0,0 +1,35 @@
+package tests;
+
+import BaseTestClass.BaseTest;
+import Categories.*;
+import org.junit.jupiter.api.Test;
+
+import static Categories.ToolbarTitle.*;
+import static Categories.SubMenuTitle.*;
+import static Categories.CheckingValueRub.*;
+import static Categories.CheckingValueRub.DISCOUNT_DOM_CLICK;
+import static Categories.InputFields.*;
+import static Categories.CheckBoxes.*;
+import static Categories.CheckingValuePercent.*;
+
+public class MortgageTest extends BaseTest {
+
+//    @Test
+//    public void mortgageTest () {
+//
+//        app.getHomePage()
+//                .closeCookiesWindow()
+//                .selectToolbarTitleAndSubMenu(MORTGAGE, MORTGAGE_FOR_SECONDARY_BUILDINGS)
+//                .checkOpenMortgagePage()
+//                .fillField(PROPERTY_VALUE,PROPERTY_VALUE_VALUE)
+//                .fillField(INITIAL_CONTRIBUTION, INITIAL_CONTRIBUTION_VALUE)
+//                .checkOrUncheckBox(LIFE_INSURANCE,TRUE)
+//                .checkOrUncheckBox(YOUNG_SEVEN,TRUE)
+//                .sleep()
+//                .checkingValueRub(DISCOUNT_DOM_CLICK, DISCOUNT_DOM_CLICK_VALUE)
+//                .checkingValueRub(MONTHLY_PAYMENTS,MONTHLY_PAYMENTS_VALUE)
+//                .checkingValueRub(REQUIRED_INCOME, REQUIRED_INCOME_VALUE)
+//                .checkingValuePercent(INTEREST_RATE, INTEREST_RATE_VALUE);
+//
+//    }
+}
