8ec76a13ed3d1e206fc9466b6aab1d799705d6c9
Gleb1908 <gleb1908@list.ru>
2021-10-12 12:20:08 +0300

    added Listener.feature

---
+++ src/main/java/utils/Listener.java
@@ -0,0 +1,31 @@
+package utils;
+
+import io.cucumber.plugin.event.*;
+import io.qameta.allure.Allure;
+import io.qameta.allure.cucumber6jvm.AllureCucumber6Jvm;
+import managers.DriverManager;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+import java.io.ByteArrayInputStream;
+
+public class Listener extends AllureCucumber6Jvm {
+
+    @Override
+    public void setEventPublisher(EventPublisher publisher) {
+
+        EventHandler<TestStepFinished> eventHandler = new EventHandler<TestStepFinished>() {
+            @Override
+            public void receive(TestStepFinished event) {
+                if (event.getResult().getStatus().is(Status.FAILED)) {
+                    byte[] byteImage = ((TakesScreenshot) DriverManager.getDriverManager().getDriver()).getScreenshotAs(OutputType.BYTES);
+                    Allure.addAttachment("Screenshot", "image/png", new ByteArrayInputStream(byteImage), null);
+                }
+            }
+        };
+        publisher.registerHandlerFor(TestStepFinished.class, eventHandler);
+        super.setEventPublisher(publisher);
+
+    }
+
+}
