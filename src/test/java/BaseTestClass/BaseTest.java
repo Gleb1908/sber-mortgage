8791aa5a07224f8f36acbe664c721d1b06ce82e6
Gleb1908 <gleb1908@list.ru>
2021-10-12 12:16:30 +0300

    added BaseTest.java

---
+++ src/test/java/BaseTestClass/BaseTest.java
@@ -0,0 +1,40 @@
+package BaseTestClass;
+
+import managers.DriverManager;
+import managers.InitManager;
+import managers.PageManager;
+import managers.TestPropManager;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+
+import static utils.PropConst.*;
+
+public class BaseTest {
+
+    /**
+     * Менеджер страничек
+     */
+    protected PageManager app = PageManager.getPageManager();
+
+    /**
+     * Менеджер WebDriver
+     */
+    private final DriverManager driverManager = DriverManager.getDriverManager();
+
+    @BeforeAll
+    public static void beforeAll() {
+        InitManager.initFramework();
+    }
+
+    @BeforeEach
+    public void beforeEach() {
+        driverManager.getDriver().get(TestPropManager.getTestPropManager().getProperty(BASE_URL));
+    }
+
+    @AfterAll
+    public static void afterAll() {
+        InitManager.quitFramework();
+    }
+
+}
